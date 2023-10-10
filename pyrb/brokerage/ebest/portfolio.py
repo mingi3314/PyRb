439735b3bef92430b3524bf1fd05fd2021a82f99
Minki Kim <68267535+mingi3314@users.noreply.github.com>
2023-10-10 23:01:09 +0900

    ⚡️ optimize api request for portfolio (#32)

--- pyrb/brokerage/ebest/portfolio.py
+++ pyrb/brokerage/ebest/portfolio.py
@@ -1,5 +1,6 @@
+from typing import Any
+
 from pydantic import NonNegativeFloat
-from requests import Response
 
 from pyrb.brokerage.base.portfolio import Portfolio, Position
 from pyrb.brokerage.ebest.client import EbestAPIClient
@@ -8,19 +9,14 @@ from pyrb.brokerage.ebest.client import EbestAPIClient
 class EbestPortfolio(Portfolio):
     def __init__(self, api_client: EbestAPIClient) -> None:
         self._api_client = api_client
+        self._serialized_portfolio: dict[str, Any] = self._fetch_portfolio()
 
     @property
     def total_value(self) -> NonNegativeFloat:
-        response = self._fetch_portfolio()
-        res = response.json()
-
-        return res["t0424OutBlock"]["sunamt"]
+        return self._serialized_portfolio["t0424OutBlock"]["sunamt"]
 
     @property
     def positions(self) -> list[Position]:
-        response = self._fetch_portfolio()
-        res = response.json()
-
         positions = [
             Position(
                 symbol=item["expcode"],
@@ -29,7 +25,7 @@ class EbestPortfolio(Portfolio):
                 average_buy_price=item["pamt"],
                 total_amount=item["appamt"],
             )
-            for item in res["t0424OutBlock1"]
+            for item in self._serialized_portfolio["t0424OutBlock1"]
         ]
 
         return positions
@@ -41,7 +37,10 @@ class EbestPortfolio(Portfolio):
     def get_position(self, symbol: str) -> Position | None:
         return next((position for position in self.positions if position.symbol == symbol), None)
 
-    def _fetch_portfolio(self) -> Response:
+    def refresh(self) -> None:
+        self._serialized_portfolio = self._fetch_portfolio()
+
+    def _fetch_portfolio(self) -> dict[str, Any]:
         """주식잔고2 TR을 조회합니다.
 
         see: https://openapi.ebestsec.co.kr/apiservice?group_id=73142d9f-1983-48d2-8543-89b75535d34c&api_id=37d22d4d-83cd-40a4-a375-81b010a4a627
@@ -61,4 +60,4 @@ class EbestPortfolio(Portfolio):
         }
 
         response = self._api_client.send_request("POST", path, headers=headers, json=body)
-        return response
+        return response.json()
