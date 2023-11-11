eeb1d3c0e6b5af042a7f1cb96340d36e8deeafcb
Minki Kim <mingi3314@gmail.com>
2023-11-11 17:05:50 +0900

    ♻️ use temp dir

--- tests/services/test_account.py
+++ tests/services/test_account.py
@@ -1,3 +1,4 @@
+import tempfile
 from pathlib import Path
 
 from pyrb.model.account import Account
@@ -7,17 +8,18 @@ from pyrb.service.account import AccountService
 
 def test_sut_set_account_with_local_config_account_repository() -> None:
     # given
-    config_path = Path("tests/services/__pycache__/test_account.py").parent / "accounts"
-    account_repo = LocalConfigAccountRepository(config_path)
-    account_service = AccountService(account_repo)
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        config_path = Path(tmpdirname) / "accounts"
+        account_repo = LocalConfigAccountRepository(config_path)
+        account_service = AccountService(account_repo)
 
-    class FakeAccount(Account):
-        foo: str
+        class FakeAccount(Account):
+            foo: str
 
-    # when
-    account = FakeAccount(foo="bar")
-    account_service.set(account)
+        # when
+        account = FakeAccount(foo="bar")
+        account_service.set(account)
 
-    # then
-    with open(config_path) as f:
-        assert f.read() == account.to_toml()
+        # then
+        with open(config_path) as f:
+            assert f.read() == account.to_toml()
