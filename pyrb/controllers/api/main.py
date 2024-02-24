a434bf145032fd48354127144907ec6244f913dd
Minki Kim <mingi3314@gmail.com>
2024-02-24 22:07:43 +0900

    ✨ 계좌 등록 API 추가

--- pyrb/controllers/api/main.py
+++ pyrb/controllers/api/main.py
@@ -1,15 +1,29 @@
+from uuid import UUID
+
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
+from starlette.status import HTTP_201_CREATED
 
 from pyrb.controllers.api.deps import AccountServiceDep
+from pyrb.enums import BrokerageType
 from pyrb.exceptions import InitializationError
-from pyrb.models.account import Account
+from pyrb.models.account import Account, AccountFactory
 
 app = FastAPI()
 
 
 class AccountResponse(BaseModel):
     account: Account
+
+
+class AccountCreateRequest(BaseModel):
+    brokerage: BrokerageType
+    app_key: str
+    secret_key: str
+
+
+class AccountCreateResponse(BaseModel):
+    account_id: UUID
 
 
 @app.get("/accounts/default", response_model=AccountResponse)
@@ -20,3 +34,15 @@ async def get_default_account(account_service: AccountServiceDep) -> AccountResp
         raise HTTPException(status_code=404, detail="No accounts registered") from e
 
     return AccountResponse(account=account)
+
+
+@app.post("/accounts", response_model=AccountCreateResponse, status_code=HTTP_201_CREATED)
+async def create_account(
+    account_service: AccountServiceDep, body: AccountCreateRequest
+) -> AccountCreateResponse:
+    account = AccountFactory.create(
+        brokerage=body.brokerage, app_key=body.app_key, app_secret=body.secret_key
+    )
+    account_service.set(account)
+
+    return AccountCreateResponse(account_id=account.id)
