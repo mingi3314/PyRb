11326bd88f240bf3fc80f66d82ba94e6ced26c78
Minki Kim <68267535+mingi3314@users.noreply.github.com>
2024-04-15 00:04:14 +0900

    ➖ Remove unused dependencies (#118)
    ### TL;DR
    
    Update frontend dependencies and remove unused FontAwesome packages.
    
    ### What changed?
    
    Removed unused FontAwesome packages and updated other dependencies.
    
    ### How to test?
    
    Ensure the frontend application still runs without any issues after the update.
    
    ### Why make this change?
    
    To improve frontend dependencies and remove unnecessary packages.
    
    ---

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,17 +2,12 @@ import { createApp } from "vue";
 import "./styles.css";
 import App from "./App.vue";
 
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { faChevronRight, faChevronDown } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import PrimeVue from 'primevue/config';
 import 'primevue/resources/themes/aura-light-green/theme.css'
 import 'primeflex/primeflex.css'
 
-library.add(faChevronRight, faChevronDown);
 const app = createApp(App);
 
-app.component('font-awesome-icon', FontAwesomeIcon);
 app.use(PrimeVue, {
     ripple: true, locale: {
         "accept": "예",
